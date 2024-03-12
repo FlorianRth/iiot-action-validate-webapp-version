524fd4eae5d6d1554837d282f661db7fd61c3f54
FlorianRth <florian.raetsch@schulz.st>
2024-03-12 15:31:39 +0100

    Added pre-head

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
+const stripVersion = (version) => {
+  // major.minor.patch
+  // major.minor.patch-preview.1
+  const parts = version.split("-");
+
+  const numbers = parts[0].split(".");
+  const major = numbers[0];
+  const minor = numbers[1];
+  const patch = numbers[2];
+
+  if (parts.length === 2 && parts[1].startsWith("preview")) {
+    const isPreview = true;
+    const previewVersion = parts[1].split(".")[1];
+    return { major, minor, patch, isPreview, previewVersion };
+  }
+
+  return {
+    major,
+    minor,
+    patch,
+    isPreview: false,
+    previewVersion: null,
+  };
+};
+
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  const prHead = core.getInput("pr-head");
+  const version = core.getInput("version");
+
+  const strippedVersion = stripVersion(version);
+
+  console.log("Major: ", stripVersion.major);
+  console.log("Minor: ", stripVersion.minor);
+  console.log("Patch: ", stripVersion.patch);
+  console.log("isPreview: ", stripVersion.isPreview);
+  console.log("previewVersion: ", stripVersion.previewVersion);
 } catch (error) {
   core.setFailed(error.message);
 }
